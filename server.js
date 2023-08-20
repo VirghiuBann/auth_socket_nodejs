a4ec51cffabe36a7a7817beacb63df9780021927
virghiu <virghiu@gmail.com>
2023-08-19 22:47:37 -0400

    feat: Serialization of a Passport User

--- server.js
+++ server.js
@@ -9,21 +9,8 @@ const { ObjectID } = require('mongodb');
 
 const app = express();
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-passport.deserializeUser((id, done) => {
-  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, null);
-  });
-});
-
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
-fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
@@ -31,12 +18,39 @@ app.use(session({
   saveUninitialized: true,
   cookie: {secure: false},
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.route('/').get((req, res) => {
-  res.render('index', { title: 'Hello', message: 'Please log in' });
+fccTesting(app); //For FCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+  
+  app.route('/').get((req, res) => {
+    res.render('index', { title: 'Connected to Database', message: 'Please login' });
+  });
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
+  });
 });
+
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
