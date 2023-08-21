0c7939d33f3e1bd771a9f9262c0fe510761c2f9e
virghiu <virghiu@gmail.com>
2023-08-21 19:30:59 -0400

    feat: create separet file to routes

---
+++ routes.js
@@ -0,0 +1,75 @@
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+
+module.exports = (app, myDataBase) => {
+  app.route('/').get((req, res) => {
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please login',
+      showLogin: true,
+      showRegistration: true
+    });
+  });
+
+  app.route('/login').post(
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res) => {
+      res.redirect('/profile');    
+     }
+  );
+
+  app.route('/logout').get(
+    (req, res) => {
+      req.logout();
+      res.redirect('/');
+    }
+  );
+
+  app.route('/profile')
+    .get(ensureAuthenticated, (req, res) => {
+      res.render('profile', {username: req.user.username});
+    });
+
+  app.route('/register')
+    .post((req, res, next) => {
+      console.log(req.body);
+      const hash = bcrypt.hashSync(req.body.password, 12);
+
+      myDataBase.findOne({ username: req.body.username }, (err, user) => {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect('/');
+        } else {
+          myDataBase.insertOne({
+            username: req.body.username,
+            password: hash
+          }, (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              next(null, doc.ops[0]);
+            }
+          });
+        }
+      }, passport.authenticate('local', { failureRedirect: '/' }),
+        (req, res, next) => {
+          res.redirect('/profile');
+        }
+      );
+    });
+  
+  function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/');
+  }
+
+    // Missing pages
+  app.use((req, res, next) => {
+    res.status(404)
+      .type("text")
+      .send('Not Found');
+  });
+};
