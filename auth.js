86ef9e171f4bf49fb0d96994d7203a0e15515d67
virghiu <virghiu@gmail.com>
2023-08-28 16:02:24 -0400

    feat: add gitHub strategy

--- auth.js
+++ auth.js
@@ -2,9 +2,21 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const bcrypt = require('bcrypt');
 const { ObjectID } = require('mongodb');
+const GitHubStrategy = require('passport-github');
 
 module.exports = (app, myDataBase) => {
-    passport.use(new LocalStrategy((username, password, done) => {
+  
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+
+  passport.use(new LocalStrategy((username, password, done) => {
     myDataBase.findOne({ username: username }, (err, user) => {
       console.log(`User ${username} attempted to log in.`);
       if (err) { return done(err); }
@@ -12,16 +24,16 @@ module.exports = (app, myDataBase) => {
       if (!bcrypt.compareSync(password, user.password)) { return done(null, false); }
       return done(null, user);
     });
-  }
-    ));
+  }));
   
-    passport.serializeUser((user, done) => {
-    done(null, user._id);
-    });
-  
-  passport.deserializeUser((id, done) => {
-    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, doc);
-    });
-  });
+  passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: 'http://localhost:8080/auth/github/callback'
+  },
+    (accessToken, refreshToken, profile, cb) => {
+      console.log(profile);
+    }
+  ));
+
 };
